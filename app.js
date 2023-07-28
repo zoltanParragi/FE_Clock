5e3785c84b6da8bb0bd81336904720cc9d0b1b68
Parragi <zparragi@gmail.com>
2023-07-28 11:15:06 +0200

    Time adjustment potion added (+5min, -10min)

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const digitalClock = document.getElementById('digitalClock');
 const quarters = document.querySelector("#quarters");
+const plus5 = document.querySelector("#plus5");
+const minus10 = document.querySelector("#minus10");
 
 (function clockBeat() {
-    const today = new Date();
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let sec = today.getSeconds();
+    const today = new Date();;
+    let adjustedDate = new Date(today.getTime() + localStorage.getItem("timeAdjustmentMin")*60*1000)
+    let hour = adjustedDate.getHours()
+    let min = adjustedDate.getMinutes()
+    let sec = adjustedDate.getSeconds();
     min = min < 10 ? ("0" + min) : min;
     sec = sec < 10 ? ("0" + sec) : sec;
     digitalClock.innerHTML = hour + ":" + min + ":" + sec;
@@ -28,10 +31,22 @@ const quarters = document.querySelector("#quarters");
     } else if (Number(min) < 30) {
         store.dispatch({ type: "SET_QUARTER", payload: "2nd quarter" });
     } else if (Number(min) < 45) {
-        store.dispatch({ type: "SET_QUARTER", payload: "3nd quarter" });
+        store.dispatch({ type: "SET_QUARTER", payload: "3rd quarter" });
     } else if (Number(min) < 60) {
         store.dispatch({ type: "SET_QUARTER", payload: "4th quarter" });
     }
 
     setTimeout(clockBeat, 1000);
 })()
+
+plus5.addEventListener("click", () => {
+    localStorage.getItem("timeAdjustmentMin") 
+    ? localStorage.setItem("timeAdjustmentMin", Number(localStorage.getItem("timeAdjustmentMin")) + 5)
+    : localStorage.setItem("timeAdjustmentMin", 5)
+});
+
+minus10.addEventListener("click", () => {
+    localStorage.getItem("timeAdjustmentMin") 
+    ? localStorage.setItem("timeAdjustmentMin", Number(localStorage.getItem("timeAdjustmentMin")) - 10)
+    : localStorage.setItem("timeAdjustmentMin", -10)
+});
